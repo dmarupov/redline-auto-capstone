667504c8c3789b885c834e09261246568d79633b
jamieguenther <jamieguenther@unomaha.edu>
2019-03-21 16:51:53 -0500

    new

--- assets/js/restfulapiscript.js
+++ assets/js/restfulapiscript.js
@@ -1,14 +1,14 @@
 var request = new XMLHttpRequest()
 
-request.open('GET', 'http://localhost:8080/users', true)
-
+request.open('GET', 'http://localhost:8080/makedropdownlst', true)
+console.log("Line 4: " + this.request.response);
 request.onload = function() {
   // Begin accessing JSON data here
   var data = JSON.parse(this.response)
- 
+
   if (request.status >= 200 && request.status < 400) {
-    data.forEach(user => {
-        console.log(user.MYUSER);
+    data.forEach(make => {
+      console.log(make.DPDN_ID + ": " + make.DPDN_DESC);
     })
   } else {
     console.log('error')
